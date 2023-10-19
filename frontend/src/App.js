bdd5157990bd0c974585a4d80b7e58bbc82a5274
ariko <philemonariko@gmail.com>
2023-10-20 00:54:46 +0300

    dd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h4 className="App-text" >Personal Website | Ariko Stephen Philemon</h4>
         <img src={settings} className="App-logo" alt="logo" />
         <p className="App-text" >This website is under construction. Please come back later.</p>
       </header>
